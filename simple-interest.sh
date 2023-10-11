4e40d8987d3fbfef4a3c3ececa2400b4d2704c16
James Neff <72764232+NeffCodes@users.noreply.github.com>
2023-10-11 15:21:46 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors: James Neff (NeffCodes)
+   # Additional Authors: NeffCodes
 
    # Input:
    # p, principal amount
